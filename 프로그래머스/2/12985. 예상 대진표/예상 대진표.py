7655d3607839311657049f1a667c5b94b37448bb
hyeongseokgo <102367212+hyeongseokgo@users.noreply.github.com>
2025-07-30 18:34:40 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,19 @@
+def solution(n,a,b):
+    
+    ro = 0
+    
+    while 1:
+        if a ==b:
+            return ro
+        
+        a = (a+1)//2
+        b = (b+1)//2
+        ro += 1
+        
+    
+    
+    return answer 
+
+
+        
+    

7e7c5358c512cf7a30c6abb8db6c10fa1fb43480
hyeongseokgo <102367212+hyeongseokgo@users.noreply.github.com>
2025-08-01 15:46:30 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,12 @@
+def solution(citations):
+    
+    citations.sort()
+    
+    answer = 0
+    
+    for i in range(len(citations)):
+        if citations[i] >= len(citations)-i:
+            answer = len(citations)-i
+            break
+    
+    return answer

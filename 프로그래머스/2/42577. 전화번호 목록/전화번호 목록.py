cba237fa15066c48488cfc5aba3801bd3b2619fb
hyeongseokgo <102367212+hyeongseokgo@users.noreply.github.com>
2025-08-04 19:38:52 +0900

    [level 2] Title: 전화번호 목록, Time: 99.60 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    
+    phone_book.sort()
+    
+    for i in range(len(phone_book)-1):
+        a = len(phone_book[i])
+        if phone_book[i+1][:a] == phone_book[i]:
+            return False
+    
+    
+    return True
